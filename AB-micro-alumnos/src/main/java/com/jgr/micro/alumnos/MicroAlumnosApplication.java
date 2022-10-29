bf9411137c48974cf37598138d4927d94025235b
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-29 13:40:28 +0200

    sá., 29 de oct. de 2022 13:40:112022-10-29 MICROSERVICIO PRINCIPAL Y MICROSERVICIO ALUMNOS

---
+++ AB-micro-alumnos/src/main/java/com/jgr/micro/alumnos/MicroAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.jgr.micro.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAlumnosApplication.class, args);
+	}
+
+}
