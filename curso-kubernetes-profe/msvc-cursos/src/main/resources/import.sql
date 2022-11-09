f4cc8f92113d035c0575abaf6a037c07d9024b4a
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-09 11:23:10 +0100

    mi.,  9 de nov. de 2022 11:22:472022-11-09 empezando de nuevo

--- curso-kubernetes-profe/msvc-cursos/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO cursos(nombre,create_At) VALUES('Curso1',NOW());
-INSERT INTO cursos(nombre,create_At) VALUES('Curso2',NOW());
-INSERT INTO cursos(nombre,create_At) VALUES('Curso3',NOW());
-INSERT INTO cursos(nombre,create_At) VALUES('Curso4',NOW());
-INSERT INTO cursos(nombre,create_At) VALUES('Curso5',NOW());
-INSERT INTO cursos_alumnos(alumno_id,curso_id) VALUES(1,1); 
