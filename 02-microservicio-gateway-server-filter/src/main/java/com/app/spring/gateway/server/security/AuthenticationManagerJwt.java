2bc1c148770c2f6ef129d8b04a3b93c292064ebe
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-09 11:39:54 +0100

    mi.,  9 de nov. de 2022 11:39:222022-11-09 ++configserver,eureka y gateway

--- 02-microservicio-gateway-server-filter/src/main/java/com/app/spring/gateway/server/security/AuthenticationManagerJwt.java
+++ 02-microservicio-gateway-server-filter/src/main/java/com/app/spring/gateway/server/security/AuthenticationManagerJwt.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
