cf1d8e61a6f1ead012bba64d58d77569d97ea93d
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-17 18:32:04 +0100

    ju., 17 de nov. de 2022 18:25:222022-11-17 ahora SI va el configserver

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-17 NO VA EL CONFIGSERVER"
+message="$(date)2022-11-17 ahora SI va el configserver"
 fi
 
 start=`date +%s`
